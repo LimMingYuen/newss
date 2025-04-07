456ca4403f4aa719bab879f4a9a4218a9e8a46be
LimMingYuen <89985930+LimMingYuen@users.noreply.github.com>
2025-04-07 22:28:45 +0800

    Delete ifx-ufe-demo-shell directory

--- ifx-ufe-demo-shell/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { ShellHomeComponent } from './shell/shell-home/shell-home.component';
-import { IfxUfeAuthGuard, LoginComponent } from 'ifx-ufe';
-
-export const routes: Routes = [
-  { path: 'home', component: ShellHomeComponent, canActivate: [IfxUfeAuthGuard] },
-  { path: '', component: LoginComponent },
-];
