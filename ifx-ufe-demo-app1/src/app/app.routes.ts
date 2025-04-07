eacc7169a426836fe7fa0636d8b38914bbce4792
LimMingYuen <89985930+LimMingYuen@users.noreply.github.com>
2025-04-07 22:27:51 +0800

    Delete ifx-ufe-demo-app1 directory

--- ifx-ufe-demo-app1/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { IfxUfeAuthGuard, LoginComponent } from 'ifx-ufe';
-import { UfeHomeComponent } from './home/home.component';
-
-export const routes: Routes = [
-  { path: 'home', component: UfeHomeComponent, canActivate: [IfxUfeAuthGuard] },
-  { path: '', component: LoginComponent },
-];
