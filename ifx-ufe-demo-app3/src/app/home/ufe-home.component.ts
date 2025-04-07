c7fb8f47369cf7f9bbccc125938a6c27668cd93c
LimMingYuen <89985930+LimMingYuen@users.noreply.github.com>
2025-04-07 22:28:35 +0800

    Delete ifx-ufe-demo-app3 directory

--- ifx-ufe-demo-app3/src/app/home/ufe-home.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'ifx-ufe-demo-home',
-  imports: [],
-  templateUrl: './ufe-home.component.html',
-  styleUrl: './ufe-home.component.scss',
-})
-export class UfeHomeComponent {}
--- ifx-ufe-demo-app3/src/app/isecure/admin-view/admin-view.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'ifx-ufe-demo-admin-view',
-  imports: [],
-  templateUrl: './admin-view.component.html',
-  styleUrl: './admin-view.component.scss',
-})
-export class AdminViewComponent {}
